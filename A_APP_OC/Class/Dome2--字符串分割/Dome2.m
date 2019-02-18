19c62649b729ff2524998ca43243c2f034d44105
赵桂安 <623916490@qq.com>
2019-02-18 17:32:55 +0800

    更细代码

--- A_APP_OC/Class/Dome2--字符串分割/Dome2.m
+++ A_APP_OC/Class/Dome2--字符串分割/Dome2.m
@@ -9,7 +9,8 @@
 #import "Dome2.h"
 
 @interface Dome2 ()
-
+@property (nonatomic, strong) UILabel * label;
+@property (nonatomic, strong) UILabel * la;
 @end
 
 @implementation Dome2
@@ -17,13 +18,57 @@
 /// 字符串分割成分成字母为key，数字为value的键值对，按value由小到大输出
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    
     // Do any additional setup after loading the view.
     self.title = @"字符串分割";
     self.view.backgroundColor = [UIColor whiteColor];
-    NSString *str = @"aaaa222bbb22mmmj333nhhhgb332ffjkjk2378aaaa222";
     
+    [self setupUI];
+    self.label.text = @"aaaa222bbb22mmmj333nhhhgb332ffjkjk2378aaaa222";
+    
+    UIBarButtonItem *barbtn=[[UIBarButtonItem alloc]initWithTitle:@"分割" style:(UIBarButtonItemStylePlain) target:self action:@selector(click)];
+    self.navigationItem.rightBarButtonItem=barbtn;
+    
+    
+}
+
+- (void)setupUI{
+    UIView * superView = self.view;
+    _label = [[UILabel alloc]init];
+    _label.numberOfLines = 0;
+    [self.view addSubview:_label];
+    [_label mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(superView).offset(kTopHeight);
+        make.left.equalTo(superView).offset(10);
+        make.right.equalTo(superView).offset(-10);
+        make.height.offset(150);
+    }];
+//    UIButton * btn = [UIButton buttonWithType:0];
+//    [btn setTitle:@"分割" forState:(UIControlStateNormal)];
+//    [btn addTarget:self action:@selector(click:) forControlEvents:(UIControlEventTouchUpInside)];
+//    [btn setTitleColor:[UIColor blackColor] forState:0];
+//    [self.view addSubview:btn];
+//    [btn mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.center.mas_equalTo(superView);
+//        make.height.offset(30);
+//        make.width.offset(200);
+//    }];
+    _la = [[UILabel alloc]init];
+    _la.numberOfLines = 0;
+    _la.textAlignment = NSTextAlignmentCenter;
+    _la.font = SYS_FONT(20);
+    [self.view addSubview:_la];
+    [_la mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(_label.mas_bottom).offset(20);
+        make.left.equalTo(superView).offset(10);
+        make.right.equalTo(superView).offset(-10);
+    }];
+}
+
+- (void)click{
     // 初始化一个scanner
-    NSScanner *scanner = [NSScanner scannerWithString:str];
+    NSScanner *scanner = [NSScanner scannerWithString:self.label.text];
     //
     NSLog(@"The string the scanner will scan: %@", scanner.string);
     NSCharacterSet *numSet = [NSCharacterSet decimalDigitCharacterSet];
@@ -72,9 +117,17 @@
             }
         }];
     }
-    
     NSLog(@"resultArr: %@", resultArr);
     
+    NSString * las = nil;
+    for (NSDictionary * dic in resultArr) {
+        if (las.length > 0) {
+            las = [NSString stringWithFormat:@"%@\n%@",las,dic];
+        }else{
+            las = [NSString stringWithFormat:@"%@",dic];
+        }
+    }
+    self.la.text = [NSString stringWithFormat:@"%@",las];
 }
 
 - (void)didReceiveMemoryWarning {
---
+++ Podfile
@@ -0,0 +1,7 @@
+platform:ios, '8.0'
+
+target 'A_APP_OC' do
+
+    pod 'Masonry', '~> 1.0.2'
+
+end
